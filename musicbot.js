957630e3764454f96072b3159d0571c75e8a69f0
Terrence Hung <terrencehung@hotmail.com>
2016-09-27 23:47:02 -0400

    play command will not work if user is not in a voice channel, added youtube streaming module

--- musicbot.js
+++ musicbot.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var Discord = require('discord.js');
+var youtubeStream = require('youtube-audio-stream');
 var bot = new Discord.Client();
 var token = null;
 
@@ -20,6 +21,41 @@ bot.on('message', msg => {
     // if not a bot command or message was sent from bot then do nothing
     if (!(msg.content.startsWith(prefix)) || msg.author.bot) {
         return;
+    }
+
+    if (msg.content.startsWith(prefix + 'play')) {
+        // print message if author is not in a voice channel
+        var msgSender = msg.author.id;
+        // get all voice channels and check if the sender is one of them
+        // only get channels from the server that the message was sent from
+        var allChannels = msg.guild.channels.array();
+        // filter for voice channels
+        var voiceChannels = [];
+        for (let i = 0; i < allChannels.length; i++) {
+            if (allChannels[i].type === 'voice') {
+                voiceChannels.push(allChannels[i]);
+            }
+        }
+        // check for the voice channel the sender is in, return if not in one
+        var channelToStream = null;
+        for (let i = 0; i < voiceChannels.length; i++) {
+            let channelMembers = voiceChannels[i].members.array();
+            let channelFound = false;
+            for (let j = 0; j < channelMembers.length; j++) {
+                if (channelMembers[i].id === msgSender) {
+                    channelToStream = voiceChannels[i];
+                    channelFound = true;
+                    break;
+                }
+            }
+            if (channelFound) {
+                break;
+            }
+        }
+        if (channelToStream === null) {
+            msg.channel.sendMessage('You must be in a voice channel to play a song.');
+            return;
+        }
     }
 });
 

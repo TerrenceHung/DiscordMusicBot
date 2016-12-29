cd148bd36932937c80dae5a1cd6214e05e232e57
Terrence Hung <terrencehung@hotmail.com>
2016-12-29 13:31:43 -0500

    refactored classes to use ES6 syntax

--- src/SongRequest.js
+++ src/SongRequest.js
@@ -1,18 +1,23 @@
 /**
- * Creates a song request.
- *
- * @constructor
- * @this {SongRequest}
- * @param {String} title The title of the song
- * @param {String} url The YouTube url of the song
- * @param {String} duration The duration of the song
- * @param {String} requester The username of the person who requested the song
+ * Class representing a song request.
  */
-function SongRequest(title, url, duration, requester) {
-    this.title = title;
-    this.url = url;
-    this.duration = duration;
-    this.requester = requester;
+class SongRequest {
+    /**
+     * Creates a song request.
+     *
+     * @constructor
+     * @this {SongRequest}
+     * @param {String} title The title of the song
+     * @param {String} url The YouTube url of the song
+     * @param {String} duration The duration of the song
+     * @param {String} requester The username of the person who requested the song
+     */
+    constructor(title, url, duration, requester) {
+        this.title = title;
+        this.url = url;
+        this.duration = duration;
+        this.requester = requester;
+    }
 }
 
 module.exports = SongRequest;
--- src/TooLongError.js
+++ src/TooLongError.js
@@ -1,10 +1,9 @@
-function TooLongError(message) {
-    this.name = 'TooLongError';
-    this.message = message || 'Song is over a day in length';
-    this.stack = (new Error()).stack;
+class TooLongError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = 'TooLongError';
+        this.message = message || 'Song is over a day in length';
+    }
 }
-
-TooLongError.prototype = Object.create(Error.prototype);
-TooLongError.prototype.constructor = TooLongError;
 
 module.exports = TooLongError;
